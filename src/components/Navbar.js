b261840a15c1e836627444051915069e5ad4e080
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-04-23 21:57:57 +0100

    updating responsive navbar, fixed styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,28 +16,38 @@ const Navbar = () => {
         setIsScrolled(false);
       }
     };
-  
-    window.addEventListener('scroll', handleScroll);
+
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-  
 
   return (
-<div className={`fixed top-0 left-0 w-full text-black shadow-md z-10 ${isScrolled ? 'bg-white' : 'bg-initialColor'}`}>
-      <nav className="container mx-auto px-6 py-3 flex justify-between items-center">
-
-        <div className="flex justify-between items-center">
+    <div
+      className={`fixed top-0 left-0 w-full text-black shadow-md z-10 ${
+        isScrolled ? "bg-white" : "bg-initialColor"
+      }`}
+    >
+      <nav className="container mx-auto px-6 py-3 flex flex-wrap justify-between items-center">
+        <div className="flex justify-between items-center w-full md:w-auto">
           <NavLink to="/" className="font-semibold text-3xl text-red-500">
             RoadReady
           </NavLink>
+
+          {/* Burger Icon */}
+          <div
+            className="md:hidden cursor-pointer"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </div>
         </div>
-        
+
         <ul
           className={`${
             isOpen ? "block" : "hidden"
-          } md:flex md:space-x-6 items-center`}
+          } md:flex md:space-x-6 items-center text-center mx-auto md:mx-0`}
         >
           <li>
             <NavLink
@@ -76,7 +86,7 @@ const Navbar = () => {
             </NavLink>
           </li>
           <li>
-            <button className="px-6 py-2 bg-red-500 hover:bg-green-600 font-semibold rounded hover:text-white">
+            <button className="px-6 py-2 bg-red-500 hover:bg-green-600 font-semibold rounded hover:text-white mb-2 md:mb-0">
               <NavLink
                 to="/sign-in"
                 activeClassName="text-gray-200"
@@ -98,13 +108,6 @@ const Navbar = () => {
             </button>
           </li>
         </ul>
-        {/* Burger Icon */}
-        <div
-          className="md:hidden cursor-pointer"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <FontAwesomeIcon icon={faBars} />{" "}
-        </div>
       </nav>
     </div>
   );
