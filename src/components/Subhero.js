a5780f268a64e9f909b530edce9b9ccf8286c79b
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-04-25 19:48:48 +0100

    fixing coa buttons to navigate on click for the whole button

--- src/components/Subhero.js
+++ src/components/Subhero.js
@@ -19,15 +19,21 @@ import { faXmark } from "@fortawesome/free-solid-svg-icons";
 const Subhero = () => {
   return (
     <>
-      <div className="container mx-auto px-4 text-center pb-114 mt-14" id="learnMore">
+      <div
+        className="container mx-auto px-4 text-center pb-114 mt-14"
+        id="learnMore"
+      >
         <h1 className="text-3xl font-bold">
           Discover the ultimate driving experience with our hassle-free car
           subscriptions.
         </h1>
         <div className="flex flex-col items-center sm:items-center md:flex-row md:justify-around mt-6">
-          
-          <div className={`flex-initial w-64 sm:h-10 mt-10 ${styles.itemSpacing}`}>
-            <h1 className="text-xl font-semibold pb-6">1. Choose your favourite vehicle</h1>
+          <div
+            className={`flex-initial w-64 sm:h-10 mt-10 ${styles.itemSpacing}`}
+          >
+            <h1 className="text-xl font-semibold pb-6">
+              1. Choose your favourite vehicle
+            </h1>
             <FontAwesomeIcon
               icon={faCar}
               style={{ color: "#ff000d" }}
@@ -35,13 +41,15 @@ const Subhero = () => {
               className="mb-6"
             />
             <p className="">
-              Choose from a wide range vehicles, from SUVs to Sports car we have it
-              available!
+              Choose from a wide range vehicles, from SUVs to Sports car we have
+              it available!
             </p>
           </div>
 
           <div className={`flex-initial w-64 mt-10 ${styles.itemSpacing}`}>
-            <h1 className="text-xl font-semibold pb-6">2. Set your preferred terms</h1>
+            <h1 className="text-xl font-semibold pb-6">
+              2. Set your preferred terms
+            </h1>
             <FontAwesomeIcon
               icon={faCalendarDays}
               style={{ color: "#ff000d" }}
@@ -67,16 +75,16 @@ const Subhero = () => {
             </p>
           </div>
         </div>
-
-        <button className="mt-8 px-6 py-2 bg-red-500 hover:bg-green-600 text-white font-semibold rounded mr-4"><NavLink
-              to="/browse"
-              activeClassName="text-gray-200"
-              className="hover:text-gray-200"
-            >
-              Lets Drive!
-            </NavLink></button>
+        <NavLink
+          to="/browse"
+          activeClassName="text-gray-200"
+          className="hover:text-gray-200"
+        >
+          <button className="mt-8 px-6 py-2 bg-red-500 hover:bg-green-600 text-white font-semibold rounded mr-4">
+            Lets Drive!
+          </button>
+        </NavLink>
       </div>
-
 
       <div className="mx-auto px-4 mt-16 pt-16 pb-6 text-center border-top bg-gray-900 text-white">
         <h1 className=" text-3xl font-bold">
@@ -89,7 +97,9 @@ const Subhero = () => {
               style={{ color: "#ff000d" }}
               size="4x"
             />
-            <p className="font-normal">Comprehensive Insurance cover for two drivers</p>
+            <p className="font-normal">
+              Comprehensive Insurance cover for two drivers
+            </p>
           </div>
 
           <div>
@@ -155,36 +165,47 @@ const Subhero = () => {
             <p>Cancel your subscription anytime</p>
           </div>
         </div>
-        <h1 className="text-center pt-20 font-bold text-2xl pb-14">We also offer many other benefits including <span className="text-red-500 font-bold">free road tax, car washes and fuel</span>.</h1>
+        <h1 className="text-center pt-20 font-bold text-2xl pb-14">
+          We also offer many other benefits including{" "}
+          <span className="text-red-500 font-bold">
+            free road tax, car washes and fuel
+          </span>
+          .
+        </h1>
       </div>
-      
+
       <div className="container mx-auto mt-6 px-5 pt-6 flex flex-col md:flex-row items-center">
-      <div className = "flex flex-col text-center items-center">
-      <h1 className="w-fukk text-3xl mx-auto items-center"> <span className="text-red-500 font-semibold">Unlock the Road:</span> Download Our App and
-      Experience Car Subscriptions Today!</h1>
-      <div className="flex flex-col md:flex-row mt-10">
-      <button
-        className="px-6 py-2 text-white font-semibold rounded mr-4 h-20 w-60 mb-6"
-        style={{
-          backgroundImage: "url('../assets/home/iosBtn.png')",
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      >
-      </button>
-      <button
-        className="px-6 py-2 text-white font-semibold rounded h-20 w-60"
-        style={{
-          backgroundImage: "url('../assets/home/androidBtn.png')",
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      >
-      </button>
-    </div>
-      </div>
-      <div className="h-[600px] w-[50%] bg-contain bg-no-repeat bg-center flex items-center" style={{ backgroundImage: "url('./assets/home/mobileApp.png')" }}>
-      </div>
+        <div className="flex flex-col text-center items-center">
+          <h1 className="w-fukk text-3xl mx-auto items-center">
+            {" "}
+            <span className="text-red-500 font-semibold">
+              Unlock the Road:
+            </span>{" "}
+            Download Our App and Experience Car Subscriptions Today!
+          </h1>
+          <div className="flex flex-col md:flex-row mt-10">
+            <button
+              className="px-6 py-2 text-white font-semibold rounded mr-4 h-20 w-60 mb-6"
+              style={{
+                backgroundImage: "url('../assets/home/iosBtn.png')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            ></button>
+            <button
+              className="px-6 py-2 text-white font-semibold rounded h-20 w-60"
+              style={{
+                backgroundImage: "url('../assets/home/androidBtn.png')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            ></button>
+          </div>
+        </div>
+        <div
+          className="h-[600px] w-[50%] bg-contain bg-no-repeat bg-center flex items-center"
+          style={{ backgroundImage: "url('./assets/home/mobileApp.png')" }}
+        ></div>
       </div>
     </>
   );
